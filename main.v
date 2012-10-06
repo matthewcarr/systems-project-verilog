8fdbfb90d1ba1b0de994df143b36fdd1a1230608
Anders Convery <adc6@hw.ac.uk>
2012-10-06 10:56:25 +0100

    Added the token dispense controller to my branch will test fully then merge it in.

--- main.v
+++ main.v
@@ -1,6 +1,18 @@
 module main (
 	input A,
 	output Z);
+	
+	reg reset;
+	reg [3:0]tokens;
+	reg start;
+	reg clock;
+	wire pwm;
+	
+	disp_x_token red (.num_token(tokens),.start(start),.clock(clock),.reset(reset),.pwm_out(pwm));
 
-	assign Z =A;
+	assign Z =pwm;
+	
+	
 endmodule
+
+
