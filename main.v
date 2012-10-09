cf470d298bd605c98da4f302a5fd5131573070c7
Anders Convery <adc6@hw.ac.uk>
2012-10-09 16:16:15 +0100

    minor edit

--- main.v
+++ main.v
@@ -2,15 +2,7 @@ module main (
 	input A,
 	output Z);
 	
-	reg reset;
-	reg [3:0]tokens;
-	reg start;
-	reg clock;
-	wire pwm;
-	
-	disp_x_token red (.num_token(tokens),.start(start),.clock(clock),.reset(reset),.pwm_out(pwm));
-
-	assign Z =pwm;
+	assign Z =A;
 	
 	
 endmodule
