e8c8f1ac6b898492df9556d6a280f32ec5ce132e
Anders <adc6@hw.ac.uk>
2012-10-28 16:05:24 +0000

    Clk-div comments added
    addeds some comments to the clock div block also included a file on how
    it work in the repo folder name docs

--- clk_div.v
+++ clk_div.v
@@ -5,8 +5,14 @@ module clk_div(
 	
 	reg [32:0]counter; 	//counter to count up to the clock pulse
 	
-	parameter clock_div=4;
+	parameter clock_div=4; //parameter that declears the divison factor
 	
+
+	//at a rising clock or falling reset one fo 3 things happens
+	//if reset is low then the counter is held at 0 untill not in the 
+	//reset phase. otherwise if the counter is currently equal to the
+	//clock division factor then it is set to 0. if neither of the
+	//above is true it is incremented by 1
 	always@(posedge old_clock or negedge reset) begin
 		if (reset==='b0) begin
 			counter <= 32'b0;
@@ -17,6 +23,6 @@ module clk_div(
 		end
 	end
 	
-	assign new_clock = (counter<=((clock_div/2)-1));
+	assign new_clock = (counter<=((clock_div/2)-1)); //when below half vaule set high
 
 endmodule
